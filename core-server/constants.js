fde5ce41484291e4162c7983045bbb50bd7a0723
iNore <charge_n@hotmail.com>
2017-10-21 21:24:30 +0700

    add payload 2

--- core-server/constants.js
+++ core-server/constants.js
@@ -1,6 +1,6 @@
 
 // exports = class CoreConstants {
-  
+
 // }
 
 /**
@@ -33,18 +33,24 @@ exports.GAME = {
 }
 
 /**
- * Payload for send message in Lobby.
+ * Payload for send message in Lobby. (Not worked)
  *
  * Name:    index 25 to 25 + 16.
  * Message: index 69 to 475. (with 0x00 seperated)
+ * Packet ID: index 9
  */
 exports.MSG_PAYLOAD = [
-  255, 5, 198, 161, 61, 54, 18, 0, 0, 42, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 3, 170, 12, 0, 0, 0,
+  255, 5, 198, 161, 61, 54, 18, 0, 0, /* PacketID */42,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, /* GameID */4,
+  170, /* mode */12, 0, 0, 0,
   /* name. */
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
 
-
+exports.MSG_PAYLOAD_2 = [
+  255, 52, 158, 188, 30, 54, 18, 0, 0, /* PacketID */1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, /* GameID */4,
+  170, /* mode */12, 0, 0, 0,
+]
